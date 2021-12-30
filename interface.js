51396dbd225a7d980a248e8311931ee51bdb80d0
Richnei <rich.nei@hotmail.com>
2021-12-30 19:47:51 -0300

    function_end

--- interface.js
+++ interface.js
@@ -12,18 +12,16 @@ function handleClick(event) {
   let square = event.target;
   let position = square.id;
 
-  handleMove(position);
-  updateSquares();
+  if (handleMove(position)) {
+    setTimeout(() => {
+      alert(" O jogo acabou - O vencedor foi " + playerTime);
+    }, 10);
+  }
+  updateSquare(position);
 }
 
-function updateSquares() {
-  let squares = document.querySelectorAll(".square");
-  squares.forEach((square) => {
-    let position = square.id;
-    let symbol = board[position];
-
-    if (symbol != "") {
-      square.innerHTML = `<div class='${symbol}'></div>`;
-    }
-  });
+function updateSquare(position) {
+  let square = document.getElementById(position.toString());
+  let symbol = board[position];
+  square.innerHTML = `<div class='${symbol}'></div>`;
 }
